b0184b6e2c9ec1aca60755acc7542375058f478b
fabricior0cha <fabriciorochapalmeira@gmail.com>
2022-11-05 11:00:31 -0300

    Primeiro commit

---
+++ src/br/com/cadastroItem/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.cadastroItem;
+
+/**
+ *
+ * @author Fabricio Rocha
+ */
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
+        
+        
+        
+    }
+}
